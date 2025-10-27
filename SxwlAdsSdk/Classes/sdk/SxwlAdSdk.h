387b4b221bf0fc2822973764f684a85be191877e
hbokok <260413992@qq.com>
2025-10-27 13:10:30 +0800

    fix

--- SxwlAdsSdk/Classes/sdk/SxwlAdSdk.h
+++ SxwlAdsSdk/Classes/sdk/SxwlAdSdk.h
@@ -6,10 +6,17 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM (NSInteger, SxwlAcceptStatus) {
+    SxwlUnknown = 0,
+    SxwlAccepted,
+    SxwlDenied,
+};
+
 @interface SxwlAdSdk : NSObject
 +(instancetype) shareInstance;
 -(void)initWithAppId:(NSString *)appId;
 -(void)setDebugMode:(BOOL)enable;
+-(void)setGDPRStatus:(SxwlAcceptStatus)status;
 -(void) startAdPrev:(NSString *)rewardId interstitialId:(NSString *)interstitialId fullScreenId:(NSString *)fullScreenId userId:(NSString *)userId;
 @end
 
